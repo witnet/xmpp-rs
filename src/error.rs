28045361f3f970ee5c6145e975dc600055c9b2f1
lumi <lumi@pew.im>
2017-02-19 20:46:44 +0100

    initial commit

---
+++ src/error.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+use std::convert::From;
+
+use xml::writer::Error as WriterError;
+use xml::reader::Error as ReaderError;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    XmlWriterError(WriterError),
+    XmlReaderError(ReaderError),
+    EndOfDocument,
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+impl From<WriterError> for Error {
+    fn from(err: WriterError) -> Error {
+        Error::XmlWriterError(err)
+    }
+}
+
+impl From<ReaderError> for Error {
+    fn from(err: ReaderError) -> Error {
+        Error::XmlReaderError(err)
+    }
+}
