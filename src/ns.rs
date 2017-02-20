8883f343b711a25ba90185884cb52e24f94e5d18
lumi <lumi@pew.im>
2017-02-20 02:05:49 +0100

    add a few namespaces

--- src/ns.rs
+++ src/ns.rs
@@ -1,3 +1,5 @@
 pub const CLIENT: &'static str = "jabber:client";
 pub const STREAM: &'static str = "http://etherx.jabber.org/streams";
 pub const TLS: &'static str = "urn:ietf:params:xml:ns:xmpp-tls";
+pub const SASL: &'static str = "urn:ietf:params:xml:ns:xmpp-sasl";
+pub const BIND: &'static str = "urn:ietf:params:xml:ns:xmpp-bind";
