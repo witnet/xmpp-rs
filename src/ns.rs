195d827bb679ccc6cdf52364541b5bf2bd047aa5
lumi <lumi@pew.im>
2017-02-21 18:17:49 +0100

    document the ns module a bit

--- src/ns.rs
+++ src/ns.rs
@@ -1,3 +1,5 @@
+//! Provides constants for namespaces.
+
 pub const CLIENT: &'static str = "jabber:client";
 pub const STREAM: &'static str = "http://etherx.jabber.org/streams";
 pub const TLS: &'static str = "urn:ietf:params:xml:ns:xmpp-tls";
