66bf10b941cd1f2601fcae85af6911fac18aa474
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2017-05-27 17:19:28 +0100

    add a stanza_debug plugin, printing every stanza on stdout

---
+++ src/plugins/stanza_debug.rs
@@ -0,0 +1,29 @@
+use plugin::PluginProxy;
+use event::{SendElement, ReceiveElement, Propagation, Priority};
+
+pub struct StanzaDebugPlugin {
+    proxy: PluginProxy,
+}
+
+impl StanzaDebugPlugin {
+    pub fn new() -> StanzaDebugPlugin {
+        StanzaDebugPlugin {
+            proxy: PluginProxy::new(),
+        }
+    }
+
+    fn handle_send_element(&self, evt: &SendElement) -> Propagation {
+        println!("[36;1mSEND[0m: {:?}", evt.0);
+        Propagation::Continue
+    }
+
+    fn handle_receive_element(&self, evt: &ReceiveElement) -> Propagation {
+        println!("[33;1mRECV[0m: {:?}", evt.0);
+        Propagation::Continue
+    }
+}
+
+impl_plugin!(StanzaDebugPlugin, proxy, [
+    (SendElement, Priority::Min) => handle_send_element,
+    (ReceiveElement, Priority::Max) => handle_receive_element,
+]);
