d6ae1fbfb7e9380fcb32a35198a905ec89e0c50c
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2018-07-31 23:08:14 +0200

    attention: Document this module.

--- src/attention.rs
+++ src/attention.rs
@@ -4,7 +4,12 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-generate_empty_element!(Attention, "attention", ATTENTION);
+#![deny(missing_docs)]
+
+generate_empty_element!(
+    /// Requests the attention of the recipient.
+    Attention, "attention", ATTENTION
+);
 
 #[cfg(test)]
 mod tests {
